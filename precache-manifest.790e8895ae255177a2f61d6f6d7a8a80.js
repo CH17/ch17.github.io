ab471c207e992a1d07e43f156d2e6b4d17fec70f
Choyon B <contact.choyon@gmail.com>
2020-02-08 15:11:40 +0600

    Updates

---
+++ precache-manifest.790e8895ae255177a2f61d6f6d7a8a80.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc030fe6327c27b4e223e4e5691a755f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d719060aabd57279f695",
+    "url": "/static/js/2.623813c4.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.623813c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "163b3dcdebb6eb0c76ba",
+    "url": "/static/js/main.d6fbf69c.chunk.js"
+  },
+  {
+    "revision": "0dda34acc7b81aad8062",
+    "url": "/static/js/runtime-main.c399816e.js"
+  }
+]);
